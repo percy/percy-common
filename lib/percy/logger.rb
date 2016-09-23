0365b9f783bf1aa418b411e56c1ef6db14bbe4fc
Mike Fotinakis <mike@fotinakis.com>
2016-09-22 20:59:41 -0700

    Update to new logging architecture.

--- lib/percy/logger.rb
+++ lib/percy/logger.rb
@@ -4,13 +4,8 @@ require 'syslog-logger'
 module Percy
   def self.logger
     @logger if defined?(@logger)
-
-    if (ENV['PERCY_ENV'] || 'development') == 'development'
-      @logger ||= Logger.new(STDOUT)
-    else
-      @logger ||= Logger::Syslog.new('percy', Syslog::LOG_LOCAL7)
-      @logger.level = Logger::INFO
-    end
+    @logger ||= Logger::Syslog.new('percy', Syslog::LOG_LOCAL7)
+    @logger.level = Logger::INFO if ENV['PERCY_ENV'] == 'production'
     @logger
   end
 end
