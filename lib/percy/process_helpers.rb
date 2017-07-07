8cb7c8b5c350c0e48a93b9ef5192c61d6c2985c4
Mike Fotinakis <mike@fotinakis.com>
2017-07-06 21:16:29 -0700

    Extract process helpers and write tests. (#2)
    * Extract process helpers and write tests.
    
    * Fix lint errors.
    
    * Add docs for Percy::ProcessHelpers.
    
    * Upgrade ruby on travis.

---
+++ lib/percy/process_helpers.rb
@@ -0,0 +1,27 @@
+require 'timeout'
+
+module Percy
+  class ProcessHelpers
+    DEFAULT_TERM_GRACE_SECONDS = 10
+
+    def self.gracefully_kill(pid, grace_period_seconds: DEFAULT_TERM_GRACE_SECONDS)
+      begin
+        Process.kill('TERM', pid)
+        Timeout.timeout(grace_period_seconds) do
+          Process.wait(pid)
+        end
+      rescue Errno::ESRCH
+        # No such process.
+        return false
+      rescue Errno::ECHILD
+        # Status has already been collected, perhaps by a Process.detach thread.
+        return false
+      rescue Timeout::Error
+        Process.kill('KILL', pid)
+        # Collect status so it doesn't stick around as zombie process.
+        Process.wait(pid, Process::WNOHANG)
+      end
+      true
+    end
+  end
+end
---
+++ spec/percy/process_helpers_spec.rb
@@ -0,0 +1,26 @@
+require 'percy/process_helpers'
+
+RSpec.describe Percy::ProcessHelpers do
+  let(:non_existent_pid) { 11111111 }
+
+  describe '#gracefully_kill' do
+    it 'returns true if the subprocess is successfully killed' do
+      pid = fork { sleep 2 }
+      expect(Percy::ProcessHelpers.gracefully_kill(pid)).to eq(true)
+    end
+    it 'returns true if the subprocess is successfully killed after timeout' do
+      pid = fork { Signal.trap(:TERM) { sleep 20 } }
+      start = Time.now
+      expect(Percy::ProcessHelpers.gracefully_kill(pid, grace_period_seconds: 0.5)).to eq(true)
+      expect(Time.now - start).to be > 0.5
+    end
+    it 'returns false if process does not exist' do
+      expect(Percy::ProcessHelpers.gracefully_kill(non_existent_pid)).to eq(false)
+    end
+    it 'returns false if exit code has already been collected' do
+      pid = fork {}
+      Process.wait(pid)
+      expect(Percy::ProcessHelpers.gracefully_kill(pid)).to eq(false)
+    end
+  end
+end
