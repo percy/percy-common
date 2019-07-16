48cac91d842cd3b68ad188fd7177ce911d3ce0cf
Paul Friedman <paulrfri@gmail.com>
2019-07-16 10:45:03 -0700

    Update Percy::Stats for dogstatsd 4.x

--- spec/percy/stats_spec.rb
+++ spec/percy/stats_spec.rb
@@ -5,37 +5,20 @@ RSpec.describe Percy::Stats do
 
   context 'without env vars' do
     it 'sets host and port from defaults' do
-      expect(stats.host).to eq('127.0.0.1')
-      expect(stats.port).to eq(8125)
+      expect(stats.connection.host).to eq '127.0.0.1'
+      expect(stats.connection.port).to eq 8125
     end
   end
 
   context 'with env vars' do
     before(:each) do
-      ENV['DATADOG_AGENT_HOST'] = 'localhost'
-      ENV['DATADOG_AGENT_PORT'] = '1000'
-    end
-    after(:each) do
-      ENV.delete('DATADOG_AGENT_HOST')
-      ENV.delete('DATADOG_AGENT_PORT')
+      stub_const 'Percy::Stats::DEFAULT_HOST', 'my.datadog.host'
+      stub_const 'Percy::Stats::DEFAULT_PORT', 1000
     end
 
-    it 'sets host and port from env vars' do
-      expect(stats.host).to eq('localhost')
-      expect(stats.port).to eq(1000)
-    end
-
-    context 'with a network failure' do
-      let(:retries_count) { 2 }
-      let(:stats) { Percy::Stats.new('foo', 1000, retry_count: retries_count, retry_delay: 0) }
-
-      it 'sets the host to localhost after retries are exceeded' do
-        expect_any_instance_of(
-          Datadog::Statsd,
-        ).to receive(:connect_to_socket).at_least(retries_count).times.and_call_original
-
-        expect(stats.host).to eq('localhost')
-      end
+    it 'sets default host and port' do
+      expect(stats.connection.host).to eq 'my.datadog.host'
+      expect(stats.connection.port).to eq 1000
     end
   end
 
@@ -52,9 +35,9 @@ RSpec.describe Percy::Stats do
   describe 'stop_timing' do
     it 'stops timing and records the time difference' do
       expect(stats).to receive(:time_since)
-        .with('foo.bar.step1', instance_of(Time), priority: :low).and_call_original
+        .with('foo.bar.step1', instance_of(Float), priority: :low).and_call_original
       expect(stats).to receive(:time_since)
-        .with('foo.bar.step2', instance_of(Time), priority: :low).and_call_original
+        .with('foo.bar.step2', instance_of(Float), priority: :low).and_call_original
 
       stats.start_timing
       stats.stop_timing('foo.bar.step1', priority: :low)
