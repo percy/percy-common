ecd3c64ffa28f3aaefaa38168f0ca0789070d44c
Faun <git@faun.me>
2020-06-09 14:29:06 -0700

    Fix Redis option parsing when using SSL-enabled connections (#34)
    * Give preference to provided options
    
    Prioritize configuration of Redis TLS keys in this order:
    
    - Use the configuration value passed in using the options hash
    - The value provided via an environment variable
    - Read the file from the provided path (via the corresponding ENV value)
    
    * Allow pre-release gem formatting
    
    * Release 3.1.2-rc.1
    
    * Allow deleting released versions
    
    * Fix yank command for rubygems
    
    * Fix incorrect method call
    
    * Release 3.1.2-rc.2
    
    * Provide certificate authority file as path only
    
    * Release 3.1.2-rc.3
    
    * Release 3.1.2
    
    * Replace hyphen with pre-release string
    
    To ensure compatibility with rubygems.org, replace the first hyphen in the
    version string with '.pre.' to ensure pre-release versions are handled
    correctly.
    
    https://github.com/rubygems/rubygems/blob/e2a4895aab3225652d5946dc34623fb4177a2678/lib/rubygems/version.rb#L221

--- release.sh
+++ release.sh
@@ -19,10 +19,15 @@ rm "$CURDIR/"percy-common*.gem >/dev/null 2>&1 || true
 delete_existing_version() {
   git tag -d "v$1" || true
   git push origin ":v$1" || true
-  gem yank "percy-common$1.gem" || true
+  # Replace - with .pre. in version string for rubygems compatibility
+  GEM_VERSION=${1/-/.pre.}
+  gem yank percy-common -v "$GEM_VERSION" || true
 }
 
-if [[ $1 == 'delete' ]]; then
+if [[ $1 =~ ^.*delete$ ]]; then
+  shift
+  echo "Preparing to delete $1"
+  sleep 3
   delete_existing_version "$PERCY_COMMON_VERSION"
 else
   CLEAN=$(
@@ -32,17 +37,24 @@ else
   if [[ "$CLEAN" == "0" ]]; then
     if [[ $1 == '--force' ]]; then
       shift
-      if [[ -n $1 ]]; then
-        echo "Deleting version $1"
+      VERSION=$1
+      if [[ -n $VERSION ]]; then
+        echo "Deleting version $VERSION"
         sleep 1
-        delete_existing_version "$1"
+        delete_existing_version "$VERSION"
       else
         echo "Missing release version"
         exit 1
       fi
     fi
     VERSION=$1
-    echo "Releasing $VERSION"
+    # Replace - with .pre. in version string for rubygems compatibility
+    GEM_VERSION="${VERSION/-/.pre.}"
+    if [[ "$GEM_VERSION" == "$VERSION" ]]; then
+      echo "Releasing $VERSION"
+    else
+      echo "Releasing $VERSION as $GEM_VERSION"
+    fi
     sleep 1
 
     sed -i "" -e "s/$PERCY_COMMON_VERSION/$VERSION/g" "lib/percy/common/version.rb"
@@ -53,9 +65,9 @@ else
     git push origin "v$VERSION" || true
 
     bundle exec rake build
-    gem push "$CURDIR/pkg/percy-common-$VERSION.gem"
+    gem push "$CURDIR/pkg/percy-common-$GEM_VERSION.gem"
     open "https://github.com/percy/percy-common/releases/new?tag=v$VERSION&title=$VERSION"
-    rm "$CURDIR/pkg/percy-common-$VERSION.gem"
+    rm "$CURDIR/pkg/percy-common-$GEM_VERSION.gem"
   else
     echo "Please commit your changes and try again"
     exit 1
